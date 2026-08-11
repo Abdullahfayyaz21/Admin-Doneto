7586f3b0fa423fe055e5784d1b9862d77cc66bbc
Abdullah Fayyaz <iabdullahfayyaz@gmail.com>
2026-08-11 15:08:36 +0500

    feat: force light theme on login page

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,16 @@
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { ThemeProviderProps } from 'next-themes/dist/types';
+import { usePathname } from 'next/navigation';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  const pathname = usePathname();
+  const isLoginPage = pathname === '/';
+
+  return (
+    <NextThemesProvider {...props} forcedTheme={isLoginPage ? 'light' : undefined}>
+      {children}
+    </NextThemesProvider>
+  );
 }
+
